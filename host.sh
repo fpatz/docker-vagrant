8bf1cf37d6e9a08136ba7e8abec87c549c2766db
Frank Patz-Brockmann <frank.patz-brockmann@contact-software.com>
2025-04-11 23:16:50 +0200

    make host.sh less chatty

--- host.sh
+++ host.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker context rm -f docker-parallels || true
+docker context rm -f docker-parallels > /dev/null
 if [ $1 == "deploy" ]; then
-    docker context import docker-parallels docker-parallels.dockercontext
-    docker context use docker-parallels
+    docker context import docker-parallels docker-parallels.dockercontext > /dev/null
+    docker context use docker-parallels > /dev/null
 fi
